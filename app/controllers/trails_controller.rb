4a55d73039317f5e966e6434c284e36918f8ccb8
LisaBee224 <lisakbuch@gmail.com>
2016-07-11 14:35:17 -0400

    hit API

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -1,12 +1,36 @@
+
 class TrailsController < ApplicationController
 
   def show
-    @trail = Trail.find_by(id: params[:id])
-    render 'show'
+
+
   end
 
   def index
     @trails = Trail.all
+response = HTTParty.get "https://trailapi-trailapi.p.mashape.com/",
+  headers:{
+    "X-Mashape-Key" => "8UWBorcFvUmshd8zqT952LbcicNSp1Ay4AmjsnCrz6ic2qCv0r",
+    "Accept" => "text/plain"
+  }
+
+    binding.pry
+    # TODO more error checking (500 error, etc)
+    json = JSON.parse(response.body)
+    json['url']
     render 'index'
+
+  end
+    def create
+    api = TrailAPI.new()
+    url = api.unique_url
+
   end
 end
+
+
+  #  response = HTTParty.get('https://trailapi-trailapi.p.mashape.com/')
+  #       headers:{
+  #   "X-Mashape-Key" => "Thtoq9CeEZmsh8IbdDP4haQGVoB8p1EV5zUjsnMD8ubOXSK7xl",
+  #   "Accept" => "text/plain"
+  # }
